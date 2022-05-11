fb7a03da39435dbbc5d4042c086708c8a42de523
MMAndresC <mandrescasabon@hotmail.com>
2022-05-11 20:41:31 +0200

    Iteracion#3

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ let z = h + y;
 let car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
+//Iteracion#3
+
+alert(10*5);
+alert(10/2);
+alert(15%9);
+y = 10;
+z = 5;
+x = 10+5;
+alert(x);
+x = 10*5;
+alert(x);
+
+
