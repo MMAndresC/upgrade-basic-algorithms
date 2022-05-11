41919831881bb49d62dabceef432d9f1ffe80321
MMAndresC <mandrescasabon@hotmail.com>
2022-05-11 20:30:31 +0200

    Iteracion#2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+//Iteracion#1
+
 let myFavoriteHero = 'Hulk';
 let x = 50;
 let h = 5;
 let y = 10;
 let z = h + y;
 
+//Iteracion#2
+
+let car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
