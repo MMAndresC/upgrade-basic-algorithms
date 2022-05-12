df3a456bff836f68b751ee939f0103e67825a5e8
MMAndresC <mandrescasabon@hotmail.com>
2022-05-12 09:43:40 +0200

    Iteracion#2 hecha completa

--- index.js
+++ index.js
@@ -15,9 +15,35 @@ let z = h + y;
 
 //Iteracion#2
 
-//1.1  ???????
+//1.1 
+
+const character = {name: 'Jack Sparrow', age: 10};
+character.age=25;
+console.log('Años de', character.name, character.age);
+
+//1.2
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log('Soy', firstName, lastName, 'tengo', age, 'años', 'y me gustan los lobos',);
+
+//1.3
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log('La suma del precio de los dos juguetes:', toy1.price + toy2.price);
+
+//1.4
+
+let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};   
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log('El precio final del primer coche es', car1.finalPrice, 'y del segundo coche', car2.finalPrice);
+
 
 //Iteracion#3
 
@@ -109,12 +135,14 @@ if ((number2/2)==number1 || (number1/5)==number3) {
 
 //1.1
 
+console.log("Nº del 0 al 9:" + '\n');
 for (let i=0; i<=9; i++){
     console.log(i);
 }
 
 //1.2
 
+console.log("Nº pares del 0 al 9:" + '\n');
 for (let i=0; i<=9; i++){
     if ((i%2)==0){
         console.log(i);
